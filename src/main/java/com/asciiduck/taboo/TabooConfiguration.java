e57e0379274a99d274c45335c7a18550a0361e35
unknown <Kristopher@.(none)>
2011-08-20 12:03:24 -0500

    Updated the pom.xml to use Bukkit's artifactory rather than local repository.

--- src/main/java/com/asciiduck/taboo/TabooConfiguration.java
+++ src/main/java/com/asciiduck/taboo/TabooConfiguration.java
@@ -8,12 +8,12 @@ public class TabooConfiguration {
 	ArrayList <Taboo> taboos;
 	public TabooConfiguration(TabooPlugin parent) {
 		Configuration config = parent.getConfiguration();
-		taboos = new ArrayList();
+		taboos = new ArrayList<Taboo>();
 		List <String> taboo_names = config.getKeys();
 		for(String name:taboo_names) {
 			Taboo t = new Taboo();
 			t.name = name;
-			String pattern_str = new String();
+			String pattern_str = "";
 			for(String s:config.getStringList(name+".patterns",null)) {
 				if(pattern_str.length()>0) {
 					pattern_str+="|";
@@ -22,14 +22,13 @@ public class TabooConfiguration {
 			}
 			parent.log.info(name+": "+pattern_str);
 			t.pattern = Pattern.compile(pattern_str,Pattern.CASE_INSENSITIVE);
-			t.actions = (ArrayList) config.getStringList(name+".actions",null);
+			t.actions = (ArrayList<String>) config.getStringList(name+".actions",null);
 
 			t.permission = config.getString(name+".permission",null);
 			if(t.permission!=null) {
 				t.permission = "taboo."+t.permission;
 			}
 			t.censor = config.getBoolean(name+".censor",false);
-
 			taboos.add(t);
 		}
 	}
